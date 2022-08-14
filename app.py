fb975264e3e8b2b31ab030d8ba33df1b2f97ca56
kayereeves <kiannarachelmarie@gmail.com>
2022-08-13 23:03:21 -0700

    heroku

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 #https://www.nintyzeros.com/2019/11/flask-mysql-crud-restful-api.html
 
 import json
+import pandas as pd
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
@@ -39,6 +40,35 @@ class Transaction(db.Model):
     def __repr__(self):
         return '' % self
 
+class ItemData(db.Model):
+    __tablename__ = "itemdata"
+    name = db.Column('Item', db.String(70), primary_key=True)
+    retirement = db.Column('retirement date', db.String(10))
+    release_type = db.Column('cap, LT buyable or RR', db.String(10))
+    num_reports = db.Column('# of reports', db.String(10))
+    values_reported = db.Column('values reported', db.String(500))
+    old_reports = db.Column('old reports', db.String(500))
+    owls_value = db.Column('last Owls value', db.String(100))
+    date_of_last_update = db.Column('last updated', db.String(20))
+
+    def create(self):
+      db.session.add(self)
+      db.session.commit()
+      return self
+
+    def __init__(self,name,retirement,release_type,num_reports,values_reported,old_reports,owls_value,date_of_last_update):
+        self.name = name
+        self.retirement = retirement
+        self.release_type = release_type
+        self.num_reports = num_reports
+        self.values_reported = values_reported
+        self.old_reports = old_reports
+        self.owls_value = owls_value
+        self.date_of_last_update = date_of_last_update
+
+    def __repr__(self):
+        return '' % self
+
 db.create_all()
 
 class TransactionSchema(SQLAlchemyAutoSchema):
@@ -52,6 +82,19 @@ class TransactionSchema(SQLAlchemyAutoSchema):
     traded_for = fields.String(required=True)
     ds = fields.String(required=False)
     notes = fields.String(required=False)
+
+class ItemDataSchema(SQLAlchemyAutoSchema):
+    class Meta(SQLAlchemyAutoSchema.Meta):
+        model = ItemData
+        sqla_session = db.session
+    name = fields.String(required=True, primary_key=True)
+    retirement = fields.String(required=False)
+    release_type = fields.String(required=False)
+    num_reports = fields.String(required=False)
+    values_reported = fields.String(required=False)
+    old_reports = fields.String(required=False)
+    owls_value = fields.String(required=False)
+    date_of_last_update = fields.String(required=False)
 
 @app.route('/transactions', methods = ['GET'])
 def index():
@@ -90,6 +133,38 @@ def get_by_item(item):
             resultList.append(transaction[i])
 
     return make_response(jsonify({"transaction": resultList}))
+
+@app.route('/itemdata', methods = ['GET'])
+def itemdata_index():
+    get_itemdata = ItemData.query.all()
+    itemdata_schema = ItemDataSchema(many=True)
+    itemdata = itemdata_schema.dump(get_itemdata)
+    return make_response(jsonify({"itemdata": itemdata}))
+
+@app.route('/itemdata/owlsvalue/<string:item>', methods = ['GET'])
+def get_owls_value(item):
+    item = item.casefold()
+    get_itemdata = ItemData.query.all()
+    itemdata_schema = ItemDataSchema(many=True)
+    itemdata = itemdata_schema.dump(get_itemdata)
+    return make_response(jsonify({"owls value": itemdata}))
+
+@app.route('/itemdata/owls_script/', methods = ['GET'])
+def owls_script():
+    get_itemdata = ItemData.query.all()
+    itemdata_schema = ItemDataSchema(many=True)
+    itemdata = itemdata_schema.dump(get_itemdata)
+    name_list = []
+    value_list = []
+
+    for data in itemdata:
+        name_list.append(data['name'])
+        value_list.append(data['owls_value'])
+
+    result = dict(zip(name_list, value_list))
+
+    return make_response(jsonify(result))
+
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run('0.0.0.0', port=443, ssl_context=('adhoc'))
