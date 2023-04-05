85378989bc120b397ed10e741f5f0774cdaf929e
kayereeves <kiannarachelmarie@gmail.com>
2023-04-05 01:41:50 -0700

    4-9

--- app/secret.py
+++ app/secret.py
@@ -1,4 +1,4 @@
 SECRET_KEY = "4e3a9d018056371e54f44e00a966fb15eddc1880791dbf64af11c3b6eb8f218b"
 DB_USER = "owls"
-DB_PASS = "vHAxYRzjZYSEr6E"
+DB_PASS = "2Du3TeJeTS7kcsf"
 DB_URL = "owls-db.c9hvuhvpnktp.us-west-2.rds.amazonaws.com/owls"
